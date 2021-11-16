d93a0061d8048be5007ea13555c36f3cb77109a1
sin0713 <narisin1124@gmail.com>
2021-11-16 12:32:41 +0000

    [Add]コントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :public do
+    get 'homes/too'
+  end
   devise_for :admins
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
