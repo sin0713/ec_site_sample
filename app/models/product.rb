fde608c377a1e9d0be2309f43d5ba743b2c3a40a
sin0713 <narisin1124@gmail.com>
2021-11-25 14:08:19 +0000

    [Add]会員側商品一覧ページ

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,10 @@ class Product < ApplicationRecord
   has_many :cart_products, dependent: :destroy
   has_many :order_details, dependent: :destroy
   belongs_to :genre
+  
+  attachment :image
+  
+  def add_tax_included_price
+    (self.tax_excluded_price * 1.10).round
+  end 
 end
